dc258449f844bcf951201b59167434f73a38a69b
llj <1029991937@qq.com>
2018-04-07 23:55:11 +0800

    完成图片加载

--- utils.py
+++ utils.py
@@ -3,6 +3,9 @@ import scipy
 import numpy as np
 import tensorflow as tf
 
+import scipy.misc as misc
+
+
 
 def load_mnist(batch_size, is_training=True):
     path = os.path.join('data', 'mnist')
@@ -37,6 +40,80 @@ def load_mnist(batch_size, is_training=True):
 
         num_te_batch = 10000 // batch_size
         return teX / 255., teY, num_te_batch
+
+
+def load_myimg(batch_size, is_training=True):
+    if is_training:
+        #目录
+        apath="data/myimg/"
+        imgall=[]
+        y=[]
+        for i in range(5):
+            dirname="附件%d/"%(i+1)
+            list_img=os.listdir(apath+dirname)
+            #排序
+            if i!=4:
+                list_img.sort(key=lambda x: int(x[4:-4]))
+            else:
+                list_img.sort()
+
+            #读取图片
+            for j in range(len(list_img)):
+                try:
+                    fd=apath+dirname+list_img[j]
+                    # loaded = np.fromfile(file=fd, dtype=np.uint8)
+                    img_in=misc.imread(fd)
+
+                    # imgall.append(loaded)
+                    imgall.append(img_in)
+                    y.append([i,j%3])#标签
+                    # i
+                    # 0=碱度0.6
+                    # 1=碱度0.8
+                    # 2=碱度1.0，
+                    # 3=碱度1.2
+                    #
+                    # j%3
+                    # 0=中心部位
+                    # 1=1/4部位
+                    # 2=边缘部位
+
+
+
+                except:
+                    pass
+
+
+
+        # trainY = loaded[8:].reshape((60000)).astype(np.int32)
+
+        trainX=imgall[:72]#前3个文件夹训练
+        trainY=y[:72]
+
+        trX = trainX / 255.
+        trY = trainY
+
+        valX = imgall[72:72+24 ] / 255.#附件4做为验证集
+        valY = y[72:72+24]
+
+        num_tr_batch = 72 // batch_size
+        num_val_batch = 24 // batch_size
+
+        return trX, trY, num_tr_batch, valX, valY, num_val_batch
+    # else:
+        # fd = open(os.path.join(path, 't10k-images-idx3-ubyte'))
+        # loaded = np.fromfile(file=fd, dtype=np.uint8)
+        # teX = loaded[16:].reshape((10000, 28, 28, 1)).astype(np.float)
+        #
+        # fd = open(os.path.join(path, 't10k-labels-idx1-ubyte'))
+        # loaded = np.fromfile(file=fd, dtype=np.uint8)
+        # teY = loaded[8:].reshape((10000)).astype(np.int32)
+        #
+        # num_te_batch = 10000 // batch_size
+        # return teX / 255., teY, num_te_batch
+
+
+
 
 
 def load_fashion_mnist(batch_size, is_training=True):
@@ -78,6 +155,8 @@ def load_data(dataset, batch_size, is_training=True, one_hot=False):
         return load_mnist(batch_size, is_training)
     elif dataset == 'fashion-mnist':
         return load_fashion_mnist(batch_size, is_training)
+    elif dataset == 'myimg':
+        return load_myimg(batch_size, is_training)
     else:
         raise Exception('Invalid dataset, please check the name of dataset:', dataset)
 
@@ -86,6 +165,8 @@ def get_batch_data(dataset, batch_size, num_threads):
     if dataset == 'mnist':
         trX, trY, num_tr_batch, valX, valY, num_val_batch = load_mnist(batch_size, is_training=True)
     elif dataset == 'fashion-mnist':
+        trX, trY, num_tr_batch, valX, valY, num_val_batch = load_fashion_mnist(batch_size, is_training=True)
+    elif dataset=="myimg":
         trX, trY, num_tr_batch, valX, valY, num_val_batch = load_fashion_mnist(batch_size, is_training=True)
     data_queues = tf.train.slice_input_producer([trX, trY])
     X, Y = tf.train.shuffle_batch(data_queues, num_threads=num_threads,
